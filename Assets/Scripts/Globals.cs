44cc3e87388758181558252147fbae68086c7794
SeeMoosse <137811216+SeeMoosse@users.noreply.github.com>
2025-03-14 21:35:01 -0400

    test

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -12,6 +12,6 @@ namespace Assets.Scripts
         public static float playerTurnRadius = 60f;
         public static float playerTurnSpeed = .4f;
 
-        public static int LastChunks = 1;
+        public static int LastChunks = 1; //comment
     }
 }
