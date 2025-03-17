73af6f534dc186d798b69b0b2291e9e0c73d4793
Keeton Ketron <tkketron@gmail.com>
2025-03-17 13:36:33 -0400

    fix merge

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -9,18 +9,12 @@ namespace Assets.Scripts
     internal static class Globals
     {
         public static float playerSpeed = 20f;
-<<<<<<< HEAD
         public static float playerTurnRadius = 30f;
         public static float playerTurnSpeed = .4f;
 
         public static int LastChunks = 5;
 
         public static int fuel = 5000;
-=======
-        public static float playerTurnRadius = 89f;
-        public static float playerTurnSpeed = .4f;
 
-        public static int LastChunks = 1; //comment
->>>>>>> b3f515fab28f6b90b929938461f1d4599544d454
     }
 }
