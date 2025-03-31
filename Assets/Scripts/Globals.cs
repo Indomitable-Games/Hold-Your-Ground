5852a85136871c4d464fc775acd9a061c5b261b2
SeeMoosse <137811216+SeeMoosse@users.noreply.github.com>
2025-03-31 01:46:24 -0400

    added resource.cs and some other stuff

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.Objects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,28 @@ namespace Assets.Scripts
 {
     internal static class Globals
     {
+        #region PlayerStats
+
         public static float playerSpeed = 20f;
         public static float playerTurnRadius = 30f;
         public static float playerTurnSpeed = .4f;
 
+        public static int fuel = 5000;
+
+        public static Resource[] playerResources = {
+            new Resource("R1", "Soft stuff", 0.1f),
+            new Resource("R2", "Medium stuff", 0.5f),
+            new Resource("R3", "Hard stuff", 0.9f)
+        };
+
+        #endregion
+
+        #region WorldGen Config
         public static int LastChunks = 3;
 
-        public static int fuel = 5000;
         public static int planetID = 0;
 
-        public static int battleDepth = -1250;
+        public static int battleDepth = int.MinValue / 2; 
+        #endregion
     }
 }
